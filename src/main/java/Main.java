1e13a00854f6a6f57c82a9fc3257677818783361
karupinka <48447965+karupinka@users.noreply.github.com>
2019-05-26 10:33:27 +0300

    Update Main.java
    Reset comment

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,8 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-
-
 public class Main {
 
     public static void main(String[] args) {
-//        WebDriver driver;
-//        System.setProperty("webdriver.gecko.driver", "C:\\Users\\gecko\\geckodriver.exe");
-//        FirefoxOptions option = new FirefoxOptions();
-//        option.setHeadless(true);
-//        driver = new FirefoxDriver(option);
 
         Menu menu = new Menu();
         menu.translate();
-
-       /* WorkWithGoogleTRans workWithGoogleTRans = new WorkWithGoogleTRans(driver);
-        System.out.println(workWithGoogleTRans.Translate("Hello", "eng"));
-        System.out.println(workWithGoogleTRans.Translate("Собака идет сюда", "rus"));*/
-
-        //workWithGoogleTRans.quit();
     }
 }
